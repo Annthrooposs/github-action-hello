5322cab2c917bfd0bdf70bfd520378c375b13fbc
Annthrooposs <44650433+Annthrooposs@users.noreply.github.com>
2022-02-08 17:39:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');  // chargement du module core du toolkit
 
 try {
-    const firstname = core.getInput('firstname');   // récupération de la valeur du paramètre
-    console.log('Hello ${firstname}!');
+    const myfirstname = core.getInput('firstname');   // récupération de la valeur du paramètre
+    console.log('Hello ${myfirstname}!');
 }
 catch (error) {
     core.setFailed(error.message);  // indique à l'agent que letraitement a échoué et lui transmet les détails du problème
