ae5f32bc071095e2f202043334f24148eaaf13b7
Annthrooposs <44650433+Annthrooposs@users.noreply.github.com>
2022-02-08 17:26:59 +0100

    The first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');  // chargement du module core du toolkit
+
+try {
+    const firstname = core.getinput('firstname');   // récupération de la valeur du paramètre
+    console.log('Hello ${firstname}!');
+}
+catch (error) {
+    core.setFailed(error.message);  // indique à l'agent que letraitement a échoué et lui transmet les détails du problème
+}
