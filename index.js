89c3452b5cf721f7a65505b944ef7178a790b1e5
Annthrooposs <44650433+Annthrooposs@users.noreply.github.com>
2022-02-08 18:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ try {
     const myfirstname = core.getInput('firstname');   // récupération de la valeur du paramètre
     console.log('Hello ${myfirstname}!');
     console.log("::warning::Cette action est obsolète, pensez à migrer sur l'action officielle XXXX/YYYY@v1")
+    core.warning("::warning::Cette action est obsolète, pensez à migrer sur l'action officielle XXXX/YYYY@v2")
 }
 catch (error) {
     core.setFailed(error.message);  // indique à l'agent que letraitement a échoué et lui transmet les détails du problème
