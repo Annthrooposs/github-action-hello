2813016c274b525532ef374c3096dc32b10a8acb
Annthrooposs <44650433+Annthrooposs@users.noreply.github.com>
2022-02-08 18:09:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');  // chargement du module core du toolkit
 try {
     const myfirstname = core.getInput('firstname');   // récupération de la valeur du paramètre
     console.log('Hello ${myfirstname}!');
+    console.log("::warning::Cette action est obsolète, pensez à migrer sur l'action officielle XXXX/YYYY@v1")
 }
 catch (error) {
     core.setFailed(error.message);  // indique à l'agent que letraitement a échoué et lui transmet les détails du problème
